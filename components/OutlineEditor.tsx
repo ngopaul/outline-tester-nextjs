3e6799d8aaba87e817179239414b042810c0df8e
Paul Ngo <paulngo123@gmail.com>
2025-02-26 14:39:37 -0800

    don't include periods and colons by default in wrap

--- components/OutlineEditor.tsx
+++ components/OutlineEditor.tsx
@@ -53,12 +53,12 @@ export default function OutlineEditor({ onDone, refreshOutlines, initialTitle, i
 
     // Find word boundaries
     let wordStart = cursorPos;
-    while (wordStart > 0 && !/\s/.test(fullText[wordStart - 1])) {
+    while (wordStart > 0 && !/\s|[.:]/.test(fullText[wordStart - 1])) {
       wordStart--;
     }
 
     let wordEnd = cursorPos;
-    while (wordEnd < fullText.length && !/\s/.test(fullText[wordEnd])) {
+    while (wordEnd < fullText.length && !/\s|[.:]/.test(fullText[wordEnd])) {
       wordEnd++;
     }
 
